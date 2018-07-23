9a50214c4f506f73da5fcfdf851e4d69c70e2fe8
Irhad Mrndzic <irhad.mrndzic@gmail.com>
2018-07-23 13:36:23 +0200

    HK

--- src/main/java/com/viber_bot/car_sharing/cofing/MVCConfig.java
+++ src/main/java/com/viber_bot/car_sharing/cofing/MVCConfig.java
@@ -9,7 +9,7 @@ public class MVCConfig implements  WebMvcConfigurer{
     public void addViewControllers(ViewControllerRegistry registry) {
        // registry.addViewController("/home").setViewName("home");
 
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("Login");
         registry.addViewController("/routes").setViewName("Routes");
     }
 }
