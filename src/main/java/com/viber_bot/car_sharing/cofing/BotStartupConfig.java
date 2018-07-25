e3438108c7b55ac4246c13c2a1e80ac03d61fc9e
Irhad Mrndzic <irhad.mrndzic@gmail.com>
2018-07-25 16:07:57 +0200

    test

--- src/main/java/com/viber_bot/car_sharing/cofing/BotStartupConfig.java
+++ src/main/java/com/viber_bot/car_sharing/cofing/BotStartupConfig.java
@@ -7,6 +7,8 @@ import com.viber.bot.api.ViberBot;
 //import com.viber_bot.car_sharing.service.ViberBotService;
 import com.viber.bot.message.TextMessage;
 import com.viber_bot.car_sharing.DemoApplication;
+import com.viber_bot.car_sharing.service.ViberBotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +24,9 @@ public class BotStartupConfig implements ApplicationListener<ApplicationReadyEve
 
     @Inject
     private ViberSignatureValidator signatureValidator;
+
+    @Autowired
+    private ViberBotService viberBotService;
 
     @Value("$(application.viber-bot.webhook-url)")
     private String webhookUrl;
@@ -41,10 +46,9 @@ public class BotStartupConfig implements ApplicationListener<ApplicationReadyEve
         // send 'Hi UserName' when conversation is started
         bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of(
                 new TextMessage("Hi " + event.getUser().getName() + ", welcome to (car)bot2share"))));
-
-//        viberBot.onMessageReceived((event, message, reponse) -> viberBotService.onMessageRecived(event, message, reponse));
-//        viberBot.onConversationStarted((event) -> viberBotService.onConversationStarted(event));
-//        viberBot.onSubscribe((event, response) -> viberBotService.onSubscribed(event, response));
-//        viberBot.onUnsubscribe((event) -> viberBotService.onUnsubcribed(event));
+        bot.onMessageReceived((event, message, reponse) -> viberBotService.onMessageReceived(event, message, reponse));
+        bot.onConversationStarted((event) -> viberBotService.onConversationStarted(event));
+       bot.onSubscribe((event, response) -> viberBotService.onSubscribe(event, response));
+       bot.onUnsubscribe((event) -> viberBotService.onUnsubscribe(event));
     }
 }
