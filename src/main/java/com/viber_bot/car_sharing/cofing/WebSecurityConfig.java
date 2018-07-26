50b35c7882764b633d89b622c60e9f9c3f26c0e9
Irhad Mrndzic <irhad.mrndzic@gmail.com>
2018-07-26 02:34:38 +0200

    ..

--- src/main/java/com/viber_bot/car_sharing/cofing/WebSecurityConfig.java
+++ src/main/java/com/viber_bot/car_sharing/cofing/WebSecurityConfig.java
@@ -19,17 +19,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/webhook").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/home")
+                .defaultSuccessUrl("/index",true)
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
+
+
     }
 
     @Bean
